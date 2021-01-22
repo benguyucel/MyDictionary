cab4ad71a84cc678164a157c86301e7363b5bf34
Yücel Bengü <taskoov@live.nl>
2021-01-22 22:43:03 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            students.Add(149, "Yücel Bengü");
+            MDictionary<int, string> mDictionary = new MDictionary<int, string>();
+            
+            mDictionary.Add(149, "Yücel Bengü");
+            mDictionary.Add(1492, "Yücel Bengü2");
+           
+
+        }
+    }
+}
